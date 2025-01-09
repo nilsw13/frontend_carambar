0ea2cf19b89e29ea3ec22ebde9507580c8daf7ab
nilsw13 <nils.wenting@laplateforme.io>
2025-01-09 17:29:38 +0100

    First commit

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+      "./index.html",
+      "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+      extend: {
+          colors: {
+              'carambar': {
+                  500: '#f97316', // Orange Carambar
+                  600: '#ea580c',
+                  700: '#c2410c',
+              }
+          }
+      }
+  },
+  plugins: [],
+}
