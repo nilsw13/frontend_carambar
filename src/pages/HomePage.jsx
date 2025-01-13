1c9cebcd4266d27e31efa1c23f962c952c77833f
nilsw13 <nils.wenting@laplateforme.io>
2025-01-14 00:27:59 +0100

    fix margin

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,17 +1,9 @@
-import React, { useCallback, useEffect, useRef } from 'react'
+import React from 'react'
 import HeroSection from '../components/Hero'
 import Actions from '../components/Actions'
-import Words from '../components/Words'
-import { motion } from 'framer-motion'
-import PrintJoke from '../components/PrintJoke'
-import { Typewriter } from 'react-simple-typewriter'
-import { RotateCcw } from 'lucide-react'
 import Separator from '../components/utils/Separator'
-import Header from '../components/Header'
-import Footer from '../components/Footer'
 import { Outlet } from 'react-router-dom'
 import { useJokes } from '../hooks/useJokes'
-import { use } from 'react'
 
 function HomePage() {
 
@@ -29,9 +21,10 @@ function HomePage() {
 
       <HeroSection/>
 
-      {/* title */}  
+       
 
-      <div className="relative flex items-center justify-center w-full">
+      <div className="relative flex items-center justify-center w-full md:mt-[20vh]">
+         {/* ligne decorative separateur */}
         <div className="absolute w-full h-px bg-gradient-to-r from-transparent via-carambar-700 to-transparent" />
         
         {/* Titre "Actions" */}
@@ -40,14 +33,14 @@ function HomePage() {
 
 
        
-     <div className='mt-[10vh] mb-[20vh] flex flex-col items-center justify-center'>
+     <div className='mt-[10vh] mb-[4vh] flex flex-col items-center justify-center'>
         <Actions/>
         
       
      </div>
 
     <section id="createDisplay">
-     <div className="w-full h-fit  max-w-4xl p-10 mt-8 text-center  rounded-lg mb-[20vh]">
+     <div className="w-full h-fit  max-w-4xl p-10  text-center  rounded-lg mb-[20vh]">
             <Outlet />
           </div>
     </section>
--- src/services/api/axios.js
+++ src/services/api/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const baseUrl =  import.meta.env.VITE_API_URL;
+
 
 
 const api = axios.create({
