e386ea65ffe803736096b96f81bc2adb9accf0ac
nilsw13 <nils.wenting@laplateforme.io>
2025-01-13 01:19:33 +0100

    sync router for deploy

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-
+import { Routes, Route } from 'react-router-dom'
 
 import HomePage from './pages/HomePage'
 
@@ -10,35 +10,43 @@ import Header from './components/Header'
 import Footer from './components/Footer'
 import Separator from './components/utils/Separator'
 
-import { HashRouter as Router, Routes, Route } from 'react-router-dom';
+
+
+
 
 function App() {
+
+  
+
   return (
-    <Router>
-      <div className="flex flex-col min-h-screen overflow-hidden bg-carambar-500">
-        {/* Header */}
-        <Header />
+    <div className="flex flex-col min-h-screen overflow-hidden bg-carambar-500 ">
+      {/* Header */}
+    <Header />
+      {/* Routes */}
+      <main className="flex-grow ">
+      <Routes>
+        <Route path="/" element={<HomePage />}>
+          <Route path="create" element={<CreateJoke />} />
+          <Route path="jokes" element={<JokeWall />} />
+        </Route>
+        <Route path="shop" element={<Shop />} />
+      </Routes>
+      </main>
 
-        {/* Routes */}
-        <main className="flex-grow">
-          <Routes>
-            <Route path="/" element={<HomePage />}>
-              <Route path="create" element={<CreateJoke />} />
-              <Route path="jokes" element={<JokeWall />} />
-            </Route>
-            <Route path="shop" element={<Shop />} />
-          </Routes>
-        </main>
+      {/* Footer */}
+      <div className="relative flex items-center justify-center w-full">
+     <div className="absolute w-full h-px bg-gradient-to-r from-transparent via-carambar-700 to-transparent" />
+     <Separator title={"Contactez nous"}/>
+     </div>
 
-        {/* Footer */}
-        <div className="relative flex items-center justify-center w-full">
-          <div className="absolute w-full h-px bg-gradient-to-r from-transparent via-carambar-700 to-transparent" />
-          <Separator title={"Contactez nous"} />
-        </div>
-        <Footer />
-      </div>
-    </Router>
-  );
+     
+
+
+      
+
+      <Footer />
+    </div>
+  )
 }
 
-export default App;
+export default App
