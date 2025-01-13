87b1ac11d80cd4d75901ae6e27a3d577417b010a
nilsw13 <nils.wenting@laplateforme.io>
2025-01-13 01:35:38 +0100

    sync router again

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
-import { HashRouter as Router, Routes, Route } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom'
+
 import HomePage from './pages/HomePage'
 
 import CreateJoke from './components/outlet-view/CreateJoke'
@@ -18,32 +19,33 @@ function App() {
   
 
   return (
-    function App() {
-      return (
-        <Router basename="/frontend_carambar">
-          <div className="flex flex-col min-h-screen overflow-hidden bg-carambar-500 ">
-            {/* Header */}
-            <Header />
-            {/* Routes */}
-            <main className="flex-grow ">
-              <Routes>
-                <Route path="/" element={<HomePage />}>
-                  <Route path="create" element={<CreateJoke />} />
-                  <Route path="jokes" element={<JokeWall />} />
-                </Route>
-                <Route path="shop" element={<Shop />} />
-              </Routes>
-            </main>
-            {/* Footer */}
-            <div className="relative flex items-center justify-center w-full">
-              <div className="absolute w-full h-px bg-gradient-to-r from-transparent via-carambar-700 to-transparent" />
-              <Separator title={"Contactez nous"} />
-            </div>
-            <Footer />
-          </div>
-        </Router>
-      );
-    }
+    <div className="flex flex-col min-h-screen overflow-hidden bg-carambar-500 ">
+      {/* Header */}
+    <Header />
+      {/* Routes */}
+      <main className="flex-grow ">
+      <Routes>
+        <Route path="/" element={<HomePage />}>
+          <Route path="create" element={<CreateJoke />} />
+          <Route path="jokes" element={<JokeWall />} />
+        </Route>
+        <Route path="shop" element={<Shop />} />
+      </Routes>
+      </main>
+
+      {/* Footer */}
+      <div className="relative flex items-center justify-center w-full">
+     <div className="absolute w-full h-px bg-gradient-to-r from-transparent via-carambar-700 to-transparent" />
+     <Separator title={"Contactez nous"}/>
+     </div>
+
+     
+
+
+      
+
+      <Footer />
+    </div>
   )
 }
 
